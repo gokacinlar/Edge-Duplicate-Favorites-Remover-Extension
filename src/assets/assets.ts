4f2634bb03eb39bbda77a9d94262d800b5277b5b
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2026-04-11 13:21:27 +0300

    fix: import order & unify scss in index.scss

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,8 @@
-// Custom SCSS
-import "./scss/globals.scss"
-import "./scss/index.scss"
 // Bootstrap & Boostrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
+// Custom SCSS
+import "./scss/index.scss"
 (window as any).bootstrap = bootstrap;
 // Remix Icons
 import "remixicon/fonts/remixicon.css";
