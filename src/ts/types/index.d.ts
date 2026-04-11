e88ab58a292878f8b42fff640ce5057d9fd5ea54
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2026-04-11 13:23:15 +0300

    add: types for templating

--- src/ts/types/index.d.ts
+++ src/ts/types/index.d.ts
@@ -1,4 +1,13 @@
-declare module '*.scss' {
-    const classes: { [key: string]: string };
-    export default classes;
+declare module "*.scss" {
+	const classes: { [key: string]: string };
+	export default classes;
+}
+
+type TemplateOptions = {
+	content: string;
+	target: HTMLElement;
+};
+
+export type {
+	TemplateOptions
 }
