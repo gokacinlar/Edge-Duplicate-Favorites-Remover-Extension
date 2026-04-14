9721c31c2d6b7f74f3ee0a672bd345d13f96c29e
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2026-04-14 14:26:51 +0300

    Update Root.ts
    add: final ui prototype

--- src/app/Root.ts
+++ src/app/Root.ts
@@ -12,21 +12,64 @@ class AppRoot extends HTMLElement {
         return /*html*/ `
             <main id="app-root" class="container-fluid gx-0 h-100 container-fluid gx-0 h-100 d-flex flex-column">
                 <section id="upperNavigation" class="row mx-0 flex-grow-0 shadow-sm">
-                    <app-navbar class="px-0 w-100"></app-navbar>
+                    <app-navbar class="d-flex flex-row align-items-center justify-content-between px-0 w-100">
+                        <a href="https://notbyai.fyi/tr/"
+                            class="link-offset-2 link-underline link-underline-opacity-0" rel="noopener noreferrer"
+                            target="_blank">
+                            <img
+                                class="img-fluid mx-1 notbyai-badge"
+                                src="../assets/images/icons/notbyai/badge-white.svg"
+                                data-bs-toggle="tooltip" data-bs-title="This extension is primarily made by a human."
+                            >
+                        </a>
+                    </app-navbar>
                 </section>
                 <section id="mainRoot" class="row mx-0 flex-grow-1">
                     <div class="d-flex flex-column w-100 h-100 align-items-center justify-content-between">
                         <div id="displayArea" class="flex-grow-1 overflow-auto">
                             this is the display area
                         </div>
-                        <div id="controlsArea" class="d-flex flex-row align-items-center justify-content-between
+                        <div id="controlsArea" class="d-flex flex-row align-items-center justify-content-evenly gap-1
                             flex-grow-0 w-100 px-2 py-2 shadow-sm">
+                            <app-button
+                                id="expandLowerMenu"
+                                type="button"
+                                role="button"
+                                class="bee-color-button bg-gradient flex-grow-3 btn btn-sm rounded-pill fs-6 fw-medium">
+                                <i class="d-flex justify-content-center remix-icon-element ri-arrow-up-s-line"></i>
+                            </app-button>
+                            <app-button
+                                id="getFavoritesButton"
+                                type="button"
+                                role="button"
+                                hasText ="Tools"
+                                class="bee-color-button bg-gradient flex-grow-2 btn btn-sm rounded-pill fs-6 fw-medium">
+                            </app-button>
+                            <app-dropdown class="dropup-center dropup">
+                                <app-button
+                                    id="bulkActionsBtn"
+                                    type="button"
+                                    role="button"
+                                    hasText ="Bulk Actions"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                    class="dropdown-toggle bee-color-button bg-gradient flex-grow-1 btn btn-sm rounded-pill
+                                        d-flex flex-row align-items-center justify-content-center gap-1 fs-6 fw-medium">
+                                </app-button>
+                                <ul class="lang-switch-list navbar-dropdown dropdown-menu rounded-4 shadow-sm">
+                                    <li class="dropdown-item"><span class="fi fi-us"></span> English</li>
+                                    <li class="dropdown-item"><span class="fi fi-de"></span> Deutsch</li>
+                                    <li class="dropdown-item"><span class="fi fi-fr"></span> French</li>
+                                    <li class="dropdown-item"><span class="fi fi-es"></span> Spanish</li>
+                                    <li class="dropdown-item"><span class="fi fi-tr"></span> Türkçe</li>
+                                </ul>
+                            </app-dropdown>
                             <app-button
                                 id="getFavoritesButton"
                                 type="button"
                                 role="button"
                                 hasText ="Get Favorites"
-                                class="bee-color-button btn btn-sm rounded-pill theme-switch-toggle-icon fs-6 fw-medium">
+                                class="bee-color-button bg-gradient flex-grow-1 btn btn-sm rounded-pill fs-6 fw-medium">
                             </app-button>
                         </div>
                     </div>
