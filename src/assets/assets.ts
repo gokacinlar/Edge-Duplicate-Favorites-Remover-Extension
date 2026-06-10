e752c04ccdacaf7c599e3fee64c0beb987662f7c
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2026-06-10 11:06:58 +0300

    add: missing semicolon

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -4,7 +4,7 @@ import * as bootstrap from "bootstrap";
 // Country Icons (for translation button related)
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 // Custom SCSS
-import "./scss/index.scss"
+import "./scss/index.scss";
 (window as any).bootstrap = bootstrap;
 // Remix Icons
 import "remixicon/fonts/remixicon.css";
